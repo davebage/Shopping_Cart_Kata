93314ccb47d2a33ba4939561304bb55decc6ccec
Dave Bage <david_bage@hotmail.com>
2022-10-24 18:58:35 +0100

    refactor tests to use multiple items

--- ParallelAndNarrowChange/Tests/ShoppingCartShould.cs
+++ ParallelAndNarrowChange/Tests/ShoppingCartShould.cs
@@ -15,20 +15,23 @@ namespace ParallelAndNarrowChange{
         [Test]
         public void calculate_the_final_price(){
             cart.Add(10);
+            cart.Add(80);
 
-            cart.CalculateTotalPrice().Should().Be(10);
+            cart.CalculateTotalPrice().Should().Be(90);
         }
 
         [Test]
         public void knows_the_number_of_items(){
             cart.Add(10);
+            cart.Add(30);
 
-            cart.NumberOfProducts().Should().Be(1);
+            cart.NumberOfProducts().Should().Be(2);
         }
 
         [Test]
         public void may_offer_discounts_when_there_at_least_one_expensive_product(){
             cart.Add(120);
+            cart.Add(10);
 
             cart.HasDiscount().Should().BeTrue();
         }
@@ -36,6 +39,7 @@ namespace ParallelAndNarrowChange{
         [Test]
         public void does_not_offer_discount_for_cheap_products(){
             cart.Add(10);
+            cart.Add(30);
 
             cart.HasDiscount().Should().BeFalse();
         }
