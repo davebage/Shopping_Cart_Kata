343855243e96576848c6ca1f2040c4ff2a460c04
Dave Bage <david_bage@hotmail.com>
2022-10-24 19:01:50 +0100

    Create new product type to contain price field.

--- ParallelAndNarrowChange/Field/ShoppingCart.cs
+++ ParallelAndNarrowChange/Field/ShoppingCart.cs
@@ -3,8 +3,18 @@ using System.Linq;
 
 namespace ParallelAndNarrowChange.Field
 {
+    public class Product
+    {
+        private readonly decimal _price;
+
+        public Product(decimal price)
+        {
+            _price = price;
+        }
+    }
     public class ShoppingCart{
         private readonly List<decimal> _prices = new List<decimal>();
+        private readonly List<Product> _products = new List<Product>();
 
         public decimal CalculateTotalPrice(){
             return _prices.Sum(price => price);
@@ -16,10 +26,11 @@ namespace ParallelAndNarrowChange.Field
 
         public void Add(int aPrice){
             _prices.Add(aPrice);
+            _products.Add(new Product(aPrice));
         }
 
         public int NumberOfProducts(){
-            return _prices.Count;
+            return _products.Count;
         }
     }
 }
